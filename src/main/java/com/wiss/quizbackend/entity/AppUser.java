a4db6d2b8e6ef1756e0c8af10fb5f9810e632970
ElodinDerBarde <r.marchese96@gmail.com>
2025-11-15 12:03:35 +0100

    AppUser vorbereitet und Lombock inkludiert

---
+++ src/main/java/com/wiss/quizbackend/entity/AppUser.java
@@ -0,0 +1,38 @@
+package com.wiss.quizbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_users")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Version
+    private Long version;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoleEnum role;
+}
+
