05aea68c6f595512c45bf4a170a864b78a2addbf
ElodinDerBarde <r.marchese96@gmail.com>
2025-11-22 11:51:56 +0100

    feat: Add AuthTest component dor Testing AuthContext

--- Frontend/quiz-frontend-mu-clean/src/pages/Home.jsx
+++ Frontend/quiz-frontend-mu-clean/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import AuthTest from "../components/auth-test.jsx";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+      <div>
+        <p>Teste hier den AuthContext:</p>
+
+        <AuthTest />
+      </div>
+  );
 };
 
 export default Home;
